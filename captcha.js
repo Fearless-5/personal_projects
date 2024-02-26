38176757e3a604b916024872df6fce7cc41c1716
Fearless-5 <160542986+Fearless-5@users.noreply.github.com>
2024-02-26 21:24:37 +0100

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -14,7 +14,7 @@ yu.onclick =()=> {
 		return;
 	}
 }
-la.innerHTML = '<label for="yu">I`m not a robot</label>';
+la.innerHTML = '<label for="yu">I`m not a robot</label><img class="12bu" src="log.png">';
 setInterval(() => {
 	if(!box_6_9.checked){
 		return;
