46fb721fb936c9348d60dad925d38eb096a1c851
Fearless-5 <160542986+Fearless-5@users.noreply.github.com>
2024-02-26 20:47:11 +0100

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -1,13 +1,16 @@
 var yu = document.getElementById("yu");
 var box_6_9 = document.getElementById("box_6_9");
 var la = document.getElementById("la");
+let rei = "";
 yu.onclick =()=> {
 	if(!box_6_9.checked){
 		box_6_9.click();
 		yu.innerHTML = "&check;";
+		rei = "a";
 	} else {
 		box_6_9.click();
 		yu.innerHTML = "";
+		rei = "";
 	}
 }
 la.innerHTML = '<label for="yu">I`m not a robot</label>';
@@ -15,6 +18,6 @@ setInterval(() => {
 	if(!box_6_9.checked){
 		return;
 	} else {
-		
+		rei = "a";
 	}
 });
