ef95c6bc739a266243786936353e2b049823e4a6
Fearless-5 <160542986+Fearless-5@users.noreply.github.com>
2024-02-26 19:41:28 +0100

    Create captcha.js

---
+++ captcha.js
@@ -0,0 +1,16 @@
+function rto(){
+	if(!box_6_9.checked){
+		box_6_9.click();
+		yu.innerHTML = "&check;";
+	} else {
+		box_6_9.click();
+		yu.innerHTML = "";
+	}
+}
+setInterval(() => {
+	if(!box_6_9.checked){
+		return;
+	} else {
+		
+	}
+});
